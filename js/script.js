2c030e18a5a9fe9ab416e04ea03e9b60d0f18219
Async <86685529+AsyncAwaite@users.noreply.github.com>
2021-07-30 21:40:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,15 +16,7 @@ window.addEventListener('DOMContentLoaded', function() {
             promo.style.marginTop = null;
         }
         }
-        // let scrollDistance = window.scrollY;
-        // if (scrollDistance >= promoHeight) {
-        //     header.classList.add('header_fixed')
-        //
-        // } else if (scrollDistance === 200) {
-        //     console.log('yes');
-        //     console.log(scrollDistance);
-            // header.classList.remove('header_fixed');
-
+   
 
 
 
